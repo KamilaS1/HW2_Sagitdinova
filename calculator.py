045aa7bfb858d196191f77fb00f6de0e3f800f58
Yan Kolesnik <kolesnik-y-i@yandex.ru>
2026-09-22 21:21:54 +0300

    Add main function

--- calculator.py
+++ calculator.py
@@ -2,14 +2,36 @@ def sum(x: float, y: float) -> float:
     """Функция для сложения двух элементов."""
     return ...
 
+
 def subtract(x: float, y: float) -> float:
     """Функция для вычитания `y` из `x`."""
     return ...
+
 
 def multiply(x: float, y: float) -> float:
     """Функция для перемножения двух элементов."""
     return ...
 
+
 def divide(x: float, y: float) -> float:
     """Функция для деления `x` на `y`."""
     return ...
+
+
+def main(input_string: str) -> float:
+    """Функция оркестратор - выполняет калькуляцию."""
+    first_elem, action, second_elem = input_string.split()
+    x, y = float(first_elem), float(second_elem)
+    if action == "+":
+        result = sum(x, y)
+    elif action == "-":
+        result = subtract(x, y)
+    elif action == "*":
+        result = multiply(x, y)
+    else:
+        result = divide(x, y)
+    return result
+
+
+if __name__ == "__main__":
+    print(main(input()))
