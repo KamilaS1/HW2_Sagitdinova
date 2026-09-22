a23baf2f75482e59752dd82a4285d2d1be660c08
Yan Kolesnik <kolesnik-y-i@yandex.ru>
2026-09-22 20:58:56 +0300

    Create bases for functions

---
+++ calculator.py
@@ -0,0 +1,15 @@
+def sum(x: float, y: float) -> float:
+    """Функция для сложения двух элементов."""
+    return ...
+
+def subtract(x: float, y: float) -> float:
+    """Функция для вычитания `y` из `x`."""
+    return ...
+
+def multiply(x: float, y: float) -> float:
+    """Функция для перемножения двух элементов."""
+    return ...
+
+def divide(x: float, y: float) -> float:
+    """Функция для деления `x` на `y`."""
+    return ...
